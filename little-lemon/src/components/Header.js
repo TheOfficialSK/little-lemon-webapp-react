4adcd9c1b29fcf8bef347a7d487ce985ee597ba6
TheSalmanK <salmankhattak2017@gmail.com>
2023-04-25 20:57:44 -0400

    stuff

---
+++ little-lemon/src/components/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <header>
+            <img src="./assets/Logo.svg" alt={"Little Lemon Logo"} width={"200"} height={"96"}/>
+        </header>
+    )
+}
+export default Header;
